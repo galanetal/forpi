95cad89a2861ccf4c442679baf73e91ddb546b72
Alan Garcia <alanadavidgarcia@gmail.com>
2014-02-13 16:16:30 -0500

    Import slit slideshow demo

--- forpi.py
+++ forpi.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/slit")
+def slit():
+    return render_template('slit.html')
+
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
